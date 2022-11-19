a84c46a982815b740b7a29b5fbd3855cd5e65314
anisha stha <stha.anisha16@gmail.com>
2022-11-19 18:46:43 +0545

    counter-app completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 const App = () => {
-    return <h1>hlo world</h1>
+    const [count, setCount] = useState(1);
+
+    return (
+        <div>
+            <p>COUNTER</p>
+            <h1>{count}</h1>
+            <button className='btn' onClick={()=>setCount(count -1)}>Decrement</button>
+            <button className='btn1' onClick={()=>setCount(0)}>Reset</button>
+            <button className='btn2' onClick={()=>setCount(count +1)}>Increment</button>
+        </div>
+    )
 }
 
 export default App;
